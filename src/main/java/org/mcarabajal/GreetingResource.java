6c95aa572d50b6c022a5e22423e05d6c8630d2c4
MarceloCarabajal <marce.tsgia@gmail.com>
2024-12-10 22:33:35 -0300

    creacion de GreetingResource para que se pueda enviar por parametro un nombre y muestre un saludo, tambien se puede hacer desde swagger. index.html: para que se pueda redirigir al swagger al entrar a localhost:8080 en vez de iniciar por defecto en la dev ui de quarkus

--- src/main/java/org/mcarabajal/GreetingResource.java
+++ src/main/java/org/mcarabajal/GreetingResource.java
@@ -2,15 +2,17 @@ package org.mcarabajal;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/greet")
 public class GreetingResource {
 
     @GET
+    @Path("/{name}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public String greet(@PathParam("name") String name) {
+        return "Hello, " + name;
     }
 }
